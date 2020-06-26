95f3106ec4e739e1a4058de5ef403d878c0acccd
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-06-26 12:21:56 -0600

    Cleaned Code
    Cleaned up code and moved location where CDC data is being pulled from

--- map.py
+++ map.py
@@ -2,24 +2,26 @@ import geopandas as gdp
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
 
-# Import map US state map data from US Census website...I downloaded the data first and imported from my computer
-us_state_map = gdp.read_file('C:/Users/huffm/Desktop/MSDS 692/Flu Shot Prediction/cb_2019_us_all_20m/cb_2019_us_state_20m/cb_2019_us_state_20m.shp')
-us_state_map.plot()
+# Import map US state map data from US Census website.  I downloaded the data first and imported from my computer but data is also on Github repository
+us_state_map = gdp.read_file('C:/Users/huffm/Desktop/MSDS 692/Flu Shot Prediction/cb_2019_us_all_20m/cb_2019_us_state_20m/cb_2019_us_state_20m.shp') # Data is in Github repository and must be downloaded and correct filepath entered for code to function
+#us_state_map.plot()  #Plot blank map
 
 # Convert 'NAME' column to uppercase to match CDC data
 us_state_map['NAME'] = us_state_map['NAME'].str.upper()
-print(us_state_map)
 # Drop Puerto Rico, Hawaii, and Alaska to keep the graph to the contiguous US in order to keep the map more compact
 us_state_map = us_state_map.drop(us_state_map.index[[1,21,50]])
 
 # Import CDC data, remove NA values and calculate ratio of H1N1 vaccines in each state
-map_features = pd.read_csv('C:/Users/huffm/Desktop/MSDS 692/Flu Shot Prediction/Data from CDC/cdc_data.csv', usecols=['VACC_H1N1_F', 'STATE'])
+zipurl = 'https://github.com/jhuffm/Huffman_MSDS-692_Project/raw/master/data/cdc_data.zip'
+with urlopen(zipurl) as zipresp:
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+        zfile.extractall('/tmp')
 
-print(map_features.head())
-print(map_features.isnull().sum())
-map_features = map_features.dropna()
-print(map_features.isnull().sum())
+map_features = pd.read_csv('/tmp/cdc_data.csv', usecols=['VACC_H1N1_F', 'STATE'])
 
 def ratio(x):
     n_1 = sum(x['VACC_H1N1_F'].values == 'Yes')
@@ -28,15 +30,11 @@ def ratio(x):
 
 by_state = map_features.groupby('STATE').apply(ratio)
 
-print(by_state.head())
 by_state = by_state.to_frame()
-print(by_state)
 by_state.rename(columns={0: 'ratio'}, inplace=True)
 
-#by_state.set_index('STATE')
 ## Merge Geo dataset with state vaccine info
 merged = pd.merge(us_state_map, by_state, left_on='NAME', right_on='STATE')
-print(merged.head())
 
 ## Create Map from Merged Dataset
 var = 'ratio'
